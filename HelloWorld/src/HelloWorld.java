999ea7d643daf7a3a35baa450844c082d4b3869a
Venom <ch_venom@126.com>
2019-07-28 18:06:55 +0800

    第一次提交 Hello World!

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author Venom
+ * @date 2019/7/28 - 下午 5:42
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
